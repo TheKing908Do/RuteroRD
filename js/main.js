c6625eee1994b0a98fb8ce9f2fb2fe201216702a
TheKing908Do <baezjoel22@gmail.com>
2025-08-23 14:45:13 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -48,26 +48,24 @@ jQuery(document).ready(function($) {
         });
 
         $('.owl-carousel').owlCarousel({
-            loop:true,
-            margin:30,
-            responsiveClass:true,
-            responsive:{
-                0:{
-                    items:1,
-                    nav:true
-                },
-                600:{
-                    items:2,
-                    nav:false
-                },
-                1000:{
-                    items:3,
-                    nav:true,
-                    loop:false
-                }
-            }
-        })
-
+    loop:true,
+    margin:30,
+    nav:true,   // 🔹 
+    dots:false, // 🔹 
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3,
+            loop:false
+        }
+    }
+})
 
 
         var contentSection = $('.content-section, .main-banner');
