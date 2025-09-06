cd0a43195aec6c65e809cae4affb8dbbf36c8563
TheKing908Do <baezjoel22@gmail.com>
2025-09-06 17:23:55 -0400

    Create main.js

---
+++ json/main.js
@@ -0,0 +1,10 @@
+// main.js
+
+// Importa lógicamente tus otros scripts
+// (si no usas bundler, solo los enlazas en el index en orden)
+console.log("RuteroRD iniciado ✅");
+
+// Ejemplo: inicializar funciones al cargar
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM cargado");
+});
